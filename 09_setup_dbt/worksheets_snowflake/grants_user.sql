cd0c305a8e8f1b48bfde152ac0819a7dc657005c
Db-Lau <debbielau.hk@gmail.com>
2026-07-27 11:56:21 +0200

    update dbt part

--- 09_setup_dbt/worksheets_snowflake/grants_user.sql
+++ 09_setup_dbt/worksheets_snowflake/grants_user.sql
@@ -3,4 +3,4 @@ USE ROLE USERADMIN;
 CREATE ROLE job_ads_dbt_role;
 
 GRANT ROLE job_ads_dbt_role TO USER transformer;
-GRANT ROLE job_ads_dbt_role TO USER debbie;
+GRANT ROLE job_ads_dbt_role TO USER ; -- Add your own user name here
